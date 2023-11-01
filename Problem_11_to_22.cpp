e847ddbc9e5ccccd614cf33cf5b6b9fcddbc3a8c
ShukhratKholmamatov <shukhratkholmamatov05@gmail.com>
2023-11-01 20:08:00 +0500

    Problem from 11 to 22

---
+++ Problem_11_to_22.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+
+    //Problem_11
+//  double a = 5.23e3;
+//  double b = 25e-4;
+//  cout<< a << endl << b;
+
+    //Problem_12
+//    int allStudents = 241;
+//    float perGroup = (float)allStudents / 11;
+//    float twoGroups = round(perGroup * 2);
+//    cout<< twoGroups << endl;
+
+    //Problem_13
+//    float side;
+//    cin >> side;
+//    float area = (3 * sqrt(3) * pow(side, 2))/2;
+//    cout<< area << endl;
+
+    //Problem_14
+//    int num;
+//    cin >> num;
+//    num++;
+//    num*=6;
+//    num-=4;
+//    num/=2;
+//    num--;
+//    num%=5;
+//    cout << num << endl;
+
+    //Problem_15
+//    char symbol = 'A';
+//    char ans = (float)symbol + 7;
+//    cout<< ans << endl;
+
+    //Problem_16
+//    float x = 13;
+//    float y = 1;
+//    float z = x++;
+//    y+=2;
+//    float n = y++;
+//    float answer = (x+z)/(y*n);
+//    cout << answer << endl;
+
+    //Problem_17
+//    int x, y;
+//    cin >> x >> y;
+//    cout << (x==y);
+
+    //Problem_18
+//    int a, b;
+//    cin >> a >> b;
+//    cout << boolalpha  << (a < 50 && a < b);
+
+    //Problem_19
+//    int a, b, c;
+//    cin >> a >> b >> c;
+//    cout << (a>b && a>c) <<endl;
+
+    //Problem_20
+//    int a, b, c, d, e;
+//    cin >> a >> b >> c >> d >> e;
+//    cout << (a>=0 || b>=0 || c>=0 || d>=0 || e>=0) << endl;
+
+    //Problem_21
+//    int a, b;
+//    cin >> a >> b;
+//    cout << (a + b > 50 && a * b > 50);
+//    cout << (a + (b-10) > 50 || a * (b-10) > 50);
+//    cout << ( a > 50 || b > 50);
+
+    //Problem_22
+//    float x, y;
+//    cin >> x >> y;
+//    cout << (y * 30.48 > x);
+
+
+
+
+
+
+
+    return 0;
+}
